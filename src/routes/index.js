09fa8e0378906034e0a25268ddebd6b9c303686c
Matias-S1lva <matiasleandro752@gmail.com>
2022-07-06 15:06:48 -0300

    puerto actualizado

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 const router = Router();
-router.get("/", (req, res) => res.render("index", {title: 'First Website with Node'}));
+router.get("/", (req, res) => res.render("index", {title: 'Portafolio | Silva Matias'}));
 
 router.get("/about", (req, res) => res.render("about", {title: 'about Me'}));
 
